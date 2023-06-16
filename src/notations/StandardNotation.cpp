48f2fb3b92e80ff8781314242bd933c50deee3f9
Aadvik <aadv1k@outlook.com>
2023-06-16 13:13:29 +0530

    chore: rename files

--- src/notations/StandardNotation.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../utils/utils.h";
-
-namespace chex {
-// "Nf3"
-StandardNotationParser::parse(const std::string notation) {
-  vector<string> tokens = splitString(notation, '');
-}
-} // namespace chex
---
+++ src/notations/StandardNotationParser.cpp
@@ -0,0 +1,9 @@
+#include "../../utils/utils.h";
+#include "./StandardNotationParser.hpp";
+
+namespace chex {
+// "Nf3"
+  StandardNotationParser::parse(const std::string notation) {
+    vector<string> tokens = splitString(notation, '');
+  }
+} // namespace chex
